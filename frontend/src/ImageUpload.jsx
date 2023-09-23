c2837bcd946167f848540e22a68ca8932f4c247f
Kaarthik-07 <57kaarthikj@gmail.com>
2023-09-23 21:46:29 +0530

    new commit

---
+++ frontend/src/ImageUpload.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const ImageUpload = () => {
+  const [imagePreview, setImagePreview] = useState(
+    'https://imgs.search.brave.com/KXXub0Ya9VhGCG-HZMEZQWrv89IHNao1y6w3ZD6JKCk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/cmQuY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIxLzA0L0dl/dHR5SW1hZ2VzLTkz/NjE3NjU0Ni5qcGc'
+  );
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setImagePreview(event.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="relative ml-24">
+      <div className="avatar-upload">
+        <div className="avatar-edit relative">
+          <input
+            type="file"
+            id="imageUpload"
+            accept=".png, .jpg, .jpeg"
+            onChange={handleImageChange}
+            className="hidden"
+          />
+          <label htmlFor="imageUpload" className="cursor-pointer">
+            <div className="w-12 h-12 bg-white border border-transparent rounded-full shadow-md hover:bg-gray-100 hover:border-gray-300 flex items-center justify-center">
+              {/* Edit Profile */}
+              <i className="fa fa-camera text-gray-700"></i>
+            </div>
+          </label>
+        </div>
+        <div className="avatar-preview w-16 h-16 rounded-full border-4 border-gray-200">
+          <div
+            id="imagePreview"
+            style={{ backgroundImage: `url(${imagePreview})` }}
+            className="w-full h-full rounded-full bg-cover bg-no-repeat bg-center"
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUpload;
--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ImageUpload from './ImageUpload';
+
 
 
 
@@ -44,7 +46,7 @@ const MainPage = () => {
 
   return (
     <>
-      <div className="bg-white-100 p-4 flex justify-between items-center shadow-lg">
+      <div className="bg-white-100 p-1/2 flex justify-between items-center shadow-lg">
         <div className="text-black text-lg font-semibold focus:outline-none">Company Name</div>
 
         <div className="w-1/3 relative">
@@ -71,10 +73,11 @@ const MainPage = () => {
           </div>
         </div>
 
-        <div className="relative">
+        <div className="relative mt-4">
           <button onClick={toggleChat} className="text-black font-semibold">
             24/7 Help
           </button>
+          
           {isChatOpen && (
             <div className="absolute right-0 mt-8 w-64 bg-white p-4 rounded shadow-lg">
               <p>AI Chatbot</p>
@@ -82,10 +85,15 @@ const MainPage = () => {
               <button onClick={closeChat} className="text-blue-500 mt-4">
                 Close
               </button>
+              
             </div>
           )}
+          <div className='relative ml-28'>My Games</div>
         </div>
+        <ImageUpload />
       </div>
+      
+      
 
       <div className="flex justify-center items-center h-screen">
         <div className="w-[934px] h-[850px] absolute right-[500px]">
@@ -121,56 +129,7 @@ const MainPage = () => {
       </div>
 
       
-<div id="default-carousel" class="relative w-full" data-carousel="slide">
-    {/* <!-- Carousel wrapper --> */}
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-         {/* <!-- Item 1 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="src/assets/images/img_1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 2 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="src/assets/images/img_2.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 3 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="src/assets/images/img_3.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 4 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="src/assets/images/img_4.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 5 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="src/assets/images/img_5.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-    </div>
-    {/* <!-- Slider indicators --> */}
-    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-    </div>
-    {/* <!-- Slider controls --> */}
-    <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
+
 
       
     </>
