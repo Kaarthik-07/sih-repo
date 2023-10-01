6abaa5cf224ad8ca7798b195ee809561ed5cfd9b
Kaarthik-07 <57kaarthikj@gmail.com>
2023-10-01 22:40:07 +0530

    added success

---
+++ frontend/src/components/SuccesContact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SuccesContact = () => {
+  return (
+    <>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+    <img src="success.gif" alt="Animated GIF" class="w-16 h-16 rounded-full"/>
+  
+  <h1 className="text-center mt-4 text-2xl font-semibold sm:text-3xl lg:text-4xl">
+    WOHOO !
+  </h1>
+  
+  <h1 className="text-center mt-2 text-sm sm:mt-4 sm:text-base lg:text-lg">
+    Your message just sent sucessfully...be active with these kinds of activities.
+    Let the world know what you've got to say.
+  </h1>
+</div>
+</>
+  )
+}
+
+export default SuccesContact;
